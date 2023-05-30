141c046aea6c03c8c3b702f3e8ae92b830b51e6b
NBDatsuya <2646809050@qq.com>
2023-05-30 18:21:24 +0800

    fix: Company tsCode type

--- src/main/java/tgkt/togastock/bean/Company.java
+++ src/main/java/tgkt/togastock/bean/Company.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class Company {
     private Integer companyId;
-    private Integer tsCode;
+    private String tsCode;
     private String exchange;
     private String chairman;
     private Double regCapital;
