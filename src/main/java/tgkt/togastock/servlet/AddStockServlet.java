1192011dc83da45972a6f8176dd9f198a95ff005
wangwenqingqq <1064078316@qq.com>
2023-05-30 21:26:12 +0800

    feat: StockListServlet.java AddStockServlet.java UserListServlet.java

--- src/main/java/tgkt/togastock/servlet/AddStockServlet.java
+++ src/main/java/tgkt/togastock/servlet/AddStockServlet.java
@@ -2,13 +2,16 @@ package tgkt.togastock.servlet;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import tgkt.togastock.bean.Stock;
+
+import tgkt.togastock.response.BaseResponse;
+import tgkt.togastock.response.StockResponse;
+
 
 import java.io.IOException;
-import java.net.Socket;
+
 
 
 @WebServlet(name = "AddStockServlet", value = "/api/addStock")
@@ -20,6 +23,21 @@ public class AddStockServlet extends BaseServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        try{
+         StockResponse.builder()
+                .ok(true)
+                .name(request.getParameter("name"))
+                .area(request.getParameter("area"))
+                .exchange(request.getParameter("exchange"))
+                .industry(request.getParameter("industry"))
+                .symbol(request.getParameter("symbol"))
+                .tsCode(request.getParameter("ts_code"))
+                .market(request.getParameter("market"))
+                .build();
+    }catch (Exception e){
+            // 错误信息
+            toJson(response, BaseResponse.builder()
+                    .ok(false).msg("增加股票信息失败，可能是数据库填写错误").build());
+        }
     }
 }
